ea5e41c636e96eea0195260737f9b6e303fb0523
Albin Aj <albinattumalil@gmail.com>
2023-06-25 10:34:07 +0530

    completed_app

---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,67 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { Observable, Subject, debounceTime, distinctUntilChanged } from 'rxjs';
+import { List } from 'src/app/model/forcast';
+import { Datas, } from 'src/app/model/rapidPlace';
+import { IWeather } from 'src/app/model/weather';
+import { PlaceAndWeatherService } from 'src/app/services/place-and-weather.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  $places!: Observable<Datas[]>;
+  searchInput!: string;
+  $current!:Observable<IWeather>
+  $forcast!:Observable<List[]>
+  forcastTime!:List[]
+  nextDays!:List[]
+  $currentForecast!:any
+  today = new Date()
+  debounce = new Subject<string>()
+
+  constructor(private service: PlaceAndWeatherService, private datePipe:DatePipe) {
+    this.debounce.pipe(debounceTime(600),
+    distinctUntilChanged()).subscribe(val=>{
+    this.search(val)
+    this.searchInput = val
+    })
+  }
+
+  search(input:string) {
+   this.$places =  this.service.getPlaces(input)
+  }
+
+  passPlace(item:Datas | any ){
+    this.searchInput = item.name
+    console.log(item.name);
+    this.$places = new Observable<Datas[]>
+    this.$current = this.service.getCurrentWeatherData(item.latitude,item.longitude)
+    this.$forcast = this.service.getForecast(item.latitude,item.longitude)
+    this.$forcast.subscribe(res=>{
+      this.forcastTime = res.filter(val => {
+      return this.datePipe.transform(val.dt_txt,'MM yyyy dd') == this.datePipe.transform(this.today,'MM yyyy dd')
+        // return a==b
+      })
+    })
+
+    this.$forcast.subscribe(res=>{
+      this.nextDays = res.filter(val=>{
+        let c = this.datePipe.transform(val.dt_txt,'HH')
+        return c == '12'
+      })
+    })
+  }
+
+
+  myLocation(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(loc=>{
+      this.passPlace(loc.coords)
+      })
+    }
+    else{alert('location not support')}
+  }
+}
---
+++ src/app/services/place-and-weather.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Datas, RootObject } from '../model/rapidPlace';
+import { IWeather } from '../model/weather';
+import { List, Rootforecast } from '../model/forcast';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaceAndWeatherService {
+
+  PLACE_BASE_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+  WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5"
+  API_KEY = "dfcf63497698e598a82dd43eee8890c9"
+
+
+  constructor(private http:HttpClient) { }
+
+   getPlaces(inputs:string):Observable<Datas[]>{
+    console.log(inputs);
+    return  this.http.get<RootObject>(`${this.PLACE_BASE_URL}/cities?minPopulation=10000&namePrefix=${inputs}`,{
+      headers:{
+        "X-RapidAPI-Key":"12fb8ea0b5mshac2ac85041f834dp192b3ejsndc50077080da",
+        "X-RapidAPI-Host":"wft-geo-db.p.rapidapi.com"
+      }
+    }).pipe(map(root=>{
+      return root.data
+    }))
+  }
+
+  getCurrentWeatherData(lat:any,lon:any):Observable<IWeather>{
+    return this.http.get<IWeather>(`${this.WEATHER_BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${this.API_KEY}&units=metric`)
+  }
+
+  getForecast(lat:any,lon:any):Observable<List[]>{
+    return this.http.get<Rootforecast>(`${this.WEATHER_BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${this.API_KEY}&units=metric`).pipe(map(val=>{
+      return val.list
+    }))
+  }
+
+
+}
